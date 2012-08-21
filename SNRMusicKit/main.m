ca91ab2891acaf2a8e6ba7972af300fe96c2f34c
Indragie Karunaratne <i@indragie.com>
2012-08-21 10:12:17 -0600

    Initial Commit

---
+++ SNRMusicKit/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SNRMusicKit
+//
+//  Created by Indragie Karunaratne on 2012-08-21.
+//  Copyright (c) 2012 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
