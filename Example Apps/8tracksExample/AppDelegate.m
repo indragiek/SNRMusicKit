7cdb9e8ed1dc70e05576301b1c5d396c54d8fdd2
Indragie Karunaratne <i@indragie.com>
2012-08-24 18:40:44 -0600

    Started creating classes for MPMediaLibrary

--- Example Apps/8tracksExample/AppDelegate.m
+++ Example Apps/8tracksExample/AppDelegate.m
@@ -7,15 +7,21 @@
 //
 
 #import "AppDelegate.h"
-//#import <SNRMusicKitMac/SMK8TracksHTTPClient.h>
+#import <SNRMusicKitMac/SMK8TracksContentSource.h>
 
 @implementation AppDelegate {
-    //SMK8TracksHTTPClient *_client;
+    SMK8TracksContentSource *_source;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-
+    _source = [SMK8TracksContentSource new];
+    [_source setAPIKey:@"API_KEY"];
+    [_source authenticateWithUsername:@"USERNAME" password:@"PASSWORD" success:^(SMK8TracksUser *user) {
+        [_source getPath:@"mixes/1002" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"%@", responseObject);
+        } failure:nil];
+    } failure:nil];
 }
 
 @end
