fd78bd0bef8d4b13ed4553978fd19cac8bd4a920
Indragie Karunaratne <i@indragie.com>
2012-08-25 23:40:23 -0600

    Wrote interface for the artwork cache

---
+++ Content Sources/Shared/SMKArtworkCache.m
@@ -0,0 +1,13 @@
+//
+//  SMKArtworkCache.m
+//  SNRMusicKit
+//
+//  Created by Indragie Karunaratne on 2012-08-25.
+//  Copyright (c) 2012 Indragie Karunaratne. All rights reserved.
+//
+
+#import "SMKArtworkCache.h"
+
+@implementation SMKArtworkCache
+
+@end
