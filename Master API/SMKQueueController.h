6187bed5d67f55401e2b0ca9480b1d9209703512
Indragie Karunaratne <i@indragie.com>
2012-08-29 13:10:28 -0600

    Finished initial API for queue controller Now to code it..

--- Master API/SMKQueueController.h
+++ Master API/SMKQueueController.h
@@ -7,7 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SMKTrack.h"
+#import "SMKPlayer.h"
+
+enum {
+    SMKQueueControllerRepeatModeNone = 0,
+    SMKQueueControllerRepeatModeAll = 1,
+    SMKQueueControllerRepeatModeOne = 2
+};
+typedef NSUInteger SMKQueueControllerRepeatMode;
 
 @interface SMKQueueController : NSObject
 
+#pragma mark - Queueing
+
+@property (nonatomic, retain, readonly) NSArray *tracks;
+- (id)initWithTracks:(NSArray *)tracks;
++ (instancetype)queueControllerWithItems:(NSArray *)tracks;
+
+- (void)insertTrack:(id<SMKTrack>)track afterTrack:(id<SMKTrack>track);
+- (void)removeAllTracks;
+- (void)removeTrack:(id<SMKTrack>)track;
+
+#pragma mark - Player
+
+@property (nonatomic, strong, readonly) id<SMKPlayer> currentPlayer;
+@property (nonatomic, strong, readonly) id<SMKTrack> currentTrack;
+@property (nonatomic, assign, readonly) NSUInteger indexOfCurrentTrack;
+@property (nonatomic, assign) float volume;
+@property (nonatomic, assign) BOOL shuffle;
+@property (nonatomic, assign) SMKQueueControllerRepeatMode repeatMode;
+
+- (IBAction)play:(id)sender;
+- (IBAction)pause:(id)sender;
+- (IBAction)playPause:(id)sender;
+- (IBAction)next:(id)sender;
+- (IBAction)previous:(id)sender;
+- (IBAction)seekForward:(id)sender;
+- (IBAction)seekBackward:(id)sender;
+
+@property (nonatomic, assign, readonly) NSTimeInterval playbackTime;
+- (void)seekToPlaybackTime:(NSTimeInterval)playbackTime;
 @end
